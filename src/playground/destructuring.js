d0ff16def15ae83d0a312576ff13bcc112534090
Pietro Balestra <pietro@google.ch>
2019-02-12 18:51:25 +0100

    Lecture 104

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,45 +1,45 @@
 //
-// Object Destruction
+// Object destructuring
 //
 
 // const person = {
-//     //name: 'Pietro',
-//     age: '28',
-//     location: {
-//         city: 'Magliaso',
-//         temp: 15
-//     }
+//   name: 'Andrew',
+//   age: 27,
+//   location: {
+//     city: 'Philadelphia',
+//     temp: 88
+//   }
 // };
 
-// const {name:firstName = 'Anonymous', age} = person;
-// console.log(`${firstName} is ${age}`);
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
 
-// const {city, temp:temperature} = person.location;
-// if (city && temperature){
-//     console.log(`It's ${temperature} is ${city}`);
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}.`);
 // }
 
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     // name: 'Penguin'
+//   }
+// };
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Tyan Holliday',
-    publisher: {
-        //name: 'Pengiun'
-    }
-}
+// const { name: publisherName = 'Self-Published' } = book.publisher;
 
-const {name:publisherName = 'Self-Published'} = book.publisher
-console.log(publisherName); 
+// console.log(publisherName); // Penguin, Self-Published
 
 //
-// Array Destructuring
+// Array destructuring
 //
-const address = ['1299 S Jupiter Street', 'Philadelphia', 'Pennsylvania', '19232'];
-const [, city, state = 'New York'] = address;
-console.log(`You are in ${city} ${state}.`);
 
-const item = ['Coffe (hot)', '$2.00', '$2.50', '$2.75'];
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
 const [itemName, , mediumPrice] = item;
 
-console.log(`A medium ${itemName} cost ${mediumPrice}`);
-
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,14 @@
-import {combineReducers, createStore} from "redux";
-import expensesReducer from '../reducers/expensesReducer';
-import filtersReducer from '../reducers/filtersReducers'
+import { createStore, combineReducers } from 'redux';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
 
 export default () => {
-    const store = createStore(
-        combineReducers({
-            expenses: expensesReducer,
-            filters: filtersReducer
-        })
-    );
-    return store;
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer
+    })
+  );
+
+  return store;
 };
