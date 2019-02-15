0eed68efa225d466605198bfae993cc981a1f7d7
Pietro Balestra <pietro@google.ch>
2019-02-15 07:41:18 +0100

    Lecture 107

--- src/app.js
+++ src/app.js
@@ -12,18 +12,17 @@ import './styles/styles.scss';
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
-
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
 const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
+import { setTextFilter, sortByDate, sortByAmount } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
   <div>
-    <input type="text" value={props.filters.text} onChange={(e) => {
-      props.dispatch(setTextFilter(e.target.value));
-    }} />
+    <input
+      type="text"
+      value={props.filters.text}
+      onChange={(e) => {
+        props.dispatch(setTextFilter(e.target.value));
+      }}
+    />
     <select
       value={props.filters.sortBy}
       onChange={(e) => {
-        switch (e.target.value) {
-          case 'amount':
-            props.dispatch(sortByAmount());
-            break;
-          case 'date':
-            props.dispatch(sortByDate());
-            break;
+        if (e.target.value === 'date') {
+          props.dispatch(sortByDate());
+        } else if (e.target.value === 'amount') {
+          props.dispatch(sortByAmount());
         }
       }}
     >
-      <option value='date'>Date</option>
+      <option value="date">Date</option>
       <option value="amount">Amount</option>
     </select>
   </div>
